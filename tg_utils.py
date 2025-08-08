c22a68841254eb4e20ae04eb5f9bc4521cfa3675
app-880-eng <app-880@outlook.com>
2025-08-09 01:46:11 +0300

    Update and rename telegram to tg_utils.py

--- tg_utils.py
+++ tg_utils.py
@@ -1,9 +1,9 @@
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-def send_telegram(msg):
+def send_telegram(text: str):
     try:
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-        requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
+        requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": text}, timeout=10)
     except Exception as e:
-        print(f"Telegram error: {e}")
+        print("Telegram error:", e)
