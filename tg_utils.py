f9d8a6834b6e34e35a308cf856f40b8b1d2d3509
app-880-eng <app-880@outlook.com>
2025-08-11 19:58:15 +0300

    Update and rename tg_utils.py to render.yaml

--- tg_utils.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import requests
-
-def send_telegram(token: str, chat_id: str, text: str):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    requests.post(url, data={"chat_id": chat_id, "text": text, "parse_mode": "Markdown"}, timeout=15)
